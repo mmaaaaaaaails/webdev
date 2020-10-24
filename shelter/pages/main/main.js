d40ffce37faaad12916a00f0e0053a3a8df977fc
Vadim <v.dudko93@gmail.com>
2020-10-25 02:12:12 +0300

    feat: add file pets.json

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -2,16 +2,16 @@
 const HAMBURGER = document.querySelector('.hamburger');
 const MENU_LIST = document.querySelector('.menu__lists');
 const LOGO = document.querySelector('.header__logo');
-const MAIN = document.querySelector('.main');
+const OVERLAY = document.querySelector('.overlay')
 const FOOTER = document.querySelector('.footer');
 
 HAMBURGER.addEventListener('click', () => {
     HAMBURGER.classList.toggle('hamburger__rotate--active');
     MENU_LIST.classList.toggle('menu__lists--active');
     LOGO.classList.toggle('header__logo--active');
-    MAIN.classList.toggle('main--active');
-    FOOTER.classList.toggle('footer--active');
+    OVERLAY.classList.toggle('overlay--active');
     document.body.style.overflow = 'hidden';
+
 });
 
 function hideMenu() {
@@ -20,11 +20,39 @@ document.addEventListener('click', (event) => {
             MENU_LIST.classList.remove('menu__lists--active');
             LOGO.classList.remove('header__logo--active');
             HAMBURGER.classList.remove('hamburger__rotate--active');
-            MAIN.classList.remove('main--active');
-            FOOTER.classList.remove('footer--active');
+            OVERLAY.classList.remove('.overlay--active');
             document.body.style.overflow = 'visible';
         }
     });
 }
 
 hideMenu();
+
+let pets = [];
+let fullPetsList = [];
+
+const request = new XMLHttpRequest();
+request.open('GET', './pets.json');
+
+request.onload = () => {
+    pets = JSON.parse(request.response);
+
+    createPets();
+}
+
+const createPets = () => {
+    const elem = document.querySelector('#app')
+    elem.innerHTML += createElement();
+}
+
+// createElement = () => {
+//     let str = '';
+//     for (let i = 0; i < pets.length; i++) {
+//         str = `<img src=" ${ pets[i].img } ">`;
+//     }
+
+//     return str;
+// }
+
+
+// request.send();
