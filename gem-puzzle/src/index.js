bdb4860b371cfc37953b95ffd43b822745f224b6
Vadim <v.dudko93@gmail.com>
2020-11-04 16:01:58 +0300

    init: start task gem-puzzle and add webpack

---
+++ gem-puzzle/src/index.js
@@ -0,0 +1,10 @@
+import './js/common';
+import './assets/css/main.css';
+
+const container = document.createElement('div');
+container.className = 'container';
+document.body.append(container);
+
+const box = document.createElement('div');
+box.className = 'box';
+container.append(box);
