d6e4473ec8f6a82da5e700f6d389fe3da51144ca
Vadim <v.dudko93@gmail.com>
2020-11-22 02:00:31 +0300

    init: start task rs-css and add webpack

---
+++ rs-css/build/webpack.base.conf.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/',
+};
+
+module.exports = {
+    externals: {
+        paths: PATHS,
+    },
+
+    entry: {
+        app: PATHS.src
+    },
+
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/',
+        }, {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+            },
+        }, {
+            test: /(wav)$/,
+            loader: 'file-loader',
+            options: {
+                outputPath: 'audio',
+                name: '[name].[ext]',
+            },
+        }, {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options: {
+            name: '[name].[ext]',
+            },
+        }, {
+            test: /\.css$/,
+            use: [
+                "style-loader",
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: { sourceMap: true }
+                },
+                {
+                loader: "postcss-loader",
+                options: {
+                    sourceMap: true,
+                    config: { path: `./postcss.config.js` }
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: './postcss.config.js' } }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: { sourceMap: true }
+                }
+            ]
+        }]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+            { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+            { from: `${PATHS.src}/${PATHS.assets}audio`, to: `${PATHS.assets}audio` },
+            { from: `${PATHS.src}/static`, to: '' },
+        ]),
+        new HtmlWebpackPlugin({
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html',
+        }),
+    ],
+};
