6401d83bc89de74ac70801d9ca6cd0be7306e4f8
Vadim <v.dudko93@gmail.com>
2020-11-26 20:09:51 +0300

    feat: add section level and hamburger

--- rs-css/src/index.js
+++ rs-css/src/index.js
@@ -1,3 +1,9 @@
 import './js/modal';
 import './assets/css/main.css';
 import './assets/scss/main.scss';
+
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('change');
+});
