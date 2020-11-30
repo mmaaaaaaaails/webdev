136a6bfd06f6e7cd4fffbd47e6ec1bd3f3edbc95
Vadim <v.dudko93@gmail.com>
2020-12-01 01:19:32 +0300

    feat: add animation for enter button

--- rs-css/src/index.js
+++ rs-css/src/index.js
@@ -2,3 +2,52 @@ import './js/modal';
 import './js/hamburger';
 import './assets/css/main.css';
 import './assets/scss/main.scss';
+
+const enter = document.querySelector('.redactor__button');
+const input = document.querySelector('.redactor__input');
+const laptop = document.querySelectorAll('.table__laptop');
+const redactor = document.querySelector('.redactor');
+
+function sortElements() {
+    laptop.forEach((element) => element.classList.add('table__laptop--well'));
+}
+
+function startAnimation() {
+    redactor.classList.add('redactor__move');
+    setTimeout(() => {
+        redactor.classList.remove('redactor__move');
+    }, 100);
+}
+
+function clickHandler() {
+    enter.addEventListener('click', () => {
+        if (input.value === 'laptop') {
+            sortElements();
+        } else {
+            startAnimation();
+        }
+    });
+}
+
+function keyDownHandler() {
+    document.addEventListener('keydown', (element) => {
+        if (input.value === 'laptop' && element.code === 'Enter') {
+            enter.classList.add('redactor__button--active');
+            sortElements();
+        } else if (element.code === 'Enter') {
+            startAnimation();
+        }
+    });
+}
+
+function keyUpHandler() {
+    document.addEventListener('keyup', (element) => {
+        if (element.code === 'Enter') {
+            enter.classList.remove('redactor__button--active');
+        }
+    });
+}
+
+clickHandler();
+keyDownHandler();
+keyUpHandler();
